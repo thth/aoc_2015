f3a6af0b49720a2e6235fbc99045e315ac3e78a1
thth <ththfh@gmail.com>
2020-12-05 15:29:54 -0800

    ohayou

---
+++ README.md
@@ -0,0 +1,7 @@
+elixir solutions for advent of code 2015 (WIP)
+
+* [2017](https://github.com/thth/aoc_2017) (elixir)
+* [2019](https://github.com/thth/aoc_2019) (elixir)
+* [2020](https://github.com/thth/aoc_2020) (elixir)
+
+![](https://user-images.githubusercontent.com/7574985/101267539-a27b6e00-370e-11eb-8391-3d82d23a4815.png)
---
+++ template.exs
@@ -0,0 +1,43 @@
+File.touch("README.md")
+File.mkdir_p!("input")
+
+~w[One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve
+Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen Twenty
+TwentyOne TwentyTwo TwentyThree TwentyFour TwentyFive]
+|> Enum.with_index(1)
+|> Enum.each(fn {word, i} ->
+  day = i |> Integer.to_string() |> String.pad_leading(2, "0")
+  content =
+    """
+    defmodule #{word} do
+      def part_one(input) do
+        input
+        |> parse()
+      end
+
+      def part_two(input) do
+        input
+        |> parse()
+      end
+
+      defp parse(text) do
+        text
+        # |> String.split("\\n")
+        # |> Enum.map(&String.to_integer/1)
+      end
+    end
+
+    input = File.read!("input/#{day}.txt")
+
+    #{word}.part_one(input)
+    |> IO.inspect()
+
+    #{word}.part_two(input)
+    |> IO.inspect()
+    """
+
+  File.touch("input/#{day}.txt")
+
+  # [:exclusive] so does not overwrite if existing
+  File.write("#{day}.exs", content, [:exclusive])
+end)
