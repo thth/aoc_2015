9614af9e235584df46c5bb5b8f6457b9f7dafb6d
thth <ththfh@gmail.com>
2020-12-11 17:59:56 -0800

    this approach also doesn't work

--- 19.exs
+++ 19.exs
@@ -1,25 +1,33 @@
 defmodule Nineteen do
+  # @start System.monotonic_time(:second)
   def part_one(input) do
     {reactions, medicine} = parse(input)
 
     MapSet.new([medicine])
     |> step_possibilities(reactions)
     |> MapSet.size()
-
-    # Enum.reduce(reactions, MapSet.new(), fn reaction, acc ->
-    #   MapSet.union(acc, possibilities(medicine, reaction))
-    # end)
-
-    # reactions
-    # |> Enum.reduce(MapSet.new(), fn reaction, acc ->
-    #   MapSet.union(acc, possibilities(medicine, reaction))
-    # end)
-    # |> MapSet.size()
   end
 
   def part_two(input) do
-    input
-    |> parse()
+    {reactions, medicine} = parse(input)
+    # done here so computed once instead of on every |> filter_possibilities
+    mirrored_reactions = Enum.map(reactions, fn {inp, out} -> {inp, Enum.reverse(out)} end)
+
+    MapSet.new([["e"]])
+    |> Stream.iterate(fn molecules ->
+     molecules
+     |> step_possibilities(reactions)
+     |> filter_possibilities(medicine, reactions, mirrored_reactions)
+    end)
+    |> Enum.find_index(&MapSet.member?(&1, medicine))
+
+    # |> Stream.with_index()
+    # |> Enum.find_value(fn {possibilities, i} ->
+    #   IO.inspect(System.monotonic_time(:second) - @start, label: "step #{i} possibilities created at")
+    #   IO.inspect(MapSet.size(possibilities), label: "size")
+    #   if MapSet.member?(possibilities, medicine), do: i, else: nil
+    #   # if medicine in possibilities, do: i, else: nil
+    # end)
   end
 
   defp parse(text) do
@@ -29,52 +37,67 @@ defmodule Nineteen do
       ~r/(\w+) => (\w+)/
       |> Regex.scan(reactions_str, capture: :all_but_first)
       |> Enum.map(fn [inp, out] -> {inp, split_molecule_str.(out)} end)
-      # |> Enum.reduce(%{}, fn [inp, out], acc ->
-      #   Map.update(acc, inp, [out], fn list -> [out | list] end)
-      # end)
     medicine = split_molecule_str.(medicine_str)
     {reactions, medicine}
   end
 
-  # defp possibilities(med, reaction), do: possibilities("", med, reaction, MapSet.new())
-
-  # defp possibilities(_past, med, {inp, _out}, mapset) when byte_size(inp) > byte_size(med), do: mapset
-  # defp possibilities(past, med, {inp, out}, mapset) do
-  #   case String.split_at(med, byte_size(inp)) do
-  #     {^inp, rest} ->
-  #       new = past <> out <> rest
-  #       possibilities(past <> inp, rest, {inp, out}, MapSet.put(mapset, new))
-  #     _ ->
-  #       {h, t} = String.split_at(med, 1)
-  #       possibilities(past <> h, t, {inp, out}, mapset)
-  #   end
-  # end
-
   defp step_possibilities(molecules, reactions) do
     Enum.reduce(molecules, MapSet.new(), fn molecule, acc ->
       Enum.reduce(reactions, acc, fn reaction, inner_acc ->
-        MapSet.union(inner_acc, possibilities(molecule, reaction))
+        MapSet.union(inner_acc, molecule_possibilities(molecule, reaction))
       end)
     end)
   end
 
-  defp possibilities(molecule, reaction), do: do_possibilities([], molecule, reaction, MapSet.new())
+  defp molecule_possibilities(molecule, reaction),
+    do: do_molecule_possibilities([], molecule, reaction, MapSet.new())
 
-  defp do_possibilities(_past, [], _reaction, mapset), do: mapset
-  defp do_possibilities(past, [inp | rest], {inp, out}, mapset),
-    do: do_possibilities(past ++ [inp], rest, {inp, out}, MapSet.put(mapset, past ++ out ++ rest))
-  defp do_possibilities(past, [particle | rest], reaction, mapset),
-    do: do_possibilities(past ++ [particle], rest, reaction, mapset)
+  defp do_molecule_possibilities(_past, [], _reaction, mapset), do: mapset
+  defp do_molecule_possibilities(past, [inp | rest], {inp, out}, mapset),
+    do: do_molecule_possibilities(past ++ [inp], rest, {inp, out}, MapSet.put(mapset, past ++ out ++ rest))
+  defp do_molecule_possibilities(past, [particle | rest], reaction, mapset),
+    do: do_molecule_possibilities(past ++ [particle], rest, reaction, mapset)
+
+  defp filter_possibilities(molecules, target, reactions, mirrored_reactions) do
+    Enum.filter(molecules, fn molecule ->
+      first_nonmatching_can_reach_target?(molecule, target, reactions)
+      and first_nonmatching_can_reach_target?(Enum.reverse(molecule), Enum.reverse(target), mirrored_reactions)
+    end)
+    |> MapSet.new()
+  end
+
+  defp first_nonmatching_can_reach_target?(molecule, target, reactions, last \\ nil)
+  defp first_nonmatching_can_reach_target?([], _rest_molecule, _reactions, _last), do: true
+  defp first_nonmatching_can_reach_target?([atom | rest_molecule], [atom | rest_target], reactions, _last),
+    do: first_nonmatching_can_reach_target?(rest_molecule, rest_target, reactions, atom)
+  defp first_nonmatching_can_reach_target?([atom | _rest_mol], [target_atom | _rest_tar], reactions, last),
+    do: ({last, [last, target_atom]} in reactions) or atom_can_reach_target?([atom], target_atom, reactions)
+
+  defp atom_can_reach_target?(atoms, target_atom, reactions, checked_atoms \\ [])
+  defp atom_can_reach_target?([], _, _, _), do: false
+  defp atom_can_reach_target?([target_atom | _], target_atom, _, _), do: true
+  defp atom_can_reach_target?([atom | rest], target_atom, reactions, checked_atoms) do
+    possible_atoms =
+      reactions
+      |> Enum.filter(fn {inp, _out} -> inp == atom end)
+      |> Enum.map(fn {_inp, out} -> hd(out) end)
+      |> Enum.reject(fn out_first_atom -> out_first_atom in checked_atoms end)
+    atom_can_reach_target?(possible_atoms ++ rest, target_atom, reactions, [atom | checked_atoms])
+  end
+
 end
 
+# :observer.start()
 input = File.read!("input/19.txt")
 # input =
 #   """
+#   e => H
+#   e => O
 #   H => HO
 #   H => OH
 #   O => HH
 
-#   HOH
+#   HOHOHO
 #   """
 #   |> String.trim()
 
@@ -82,6 +105,6 @@ input
 |> Nineteen.part_one()
 |> IO.inspect()
 
-# input
-# |> Nineteen.part_two()
-# |> IO.inspect()
+input
+|> Nineteen.part_two()
+|> IO.inspect()
